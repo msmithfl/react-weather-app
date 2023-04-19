2a73e92d130cf6c44b498f7281cbd782dae4985f
Matt Smith <msmithfl@icloud.com>
2023-04-19 13:11:38 -0400

    current weather implemented

--- src/api.js
+++ src/api.js
@@ -7,3 +7,6 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3acc2f32fddc9aca1d1f4293f635d761";
